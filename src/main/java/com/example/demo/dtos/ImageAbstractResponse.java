6c9362660d77a17447f2a0ca3b6e0f94049cc130
An Nguyen Ba <an.nguyen@bstarsolutions.com>
2022-11-10 12:01:04 +0700

    commit

--- src/main/java/com/example/demo/dtos/ImageAbstractResponse.java
+++ src/main/java/com/example/demo/dtos/ImageAbstractResponse.java
@@ -18,48 +18,10 @@ public class ImageAbstractResponse {
     private String message = "OK";
     private int message_code = 200;
     private String message_type;
-    private Object data;
     private Paging meta = null;
     private Object imageUrl;
 
-    public ImageAbstractResponse(AbstractMap.SimpleEntry<Object, Paging> dataMeta) {
-        this.data = dataMeta.getKey();
-        this.meta = dataMeta.getValue();
-    }
-
-    public ImageAbstractResponse(Object data) {
-        this.imageUrl = data;
-    }
-
-    public ImageAbstractResponse(Object data, Paging paging) {
-        this.data = data;
-        this.meta = paging;
-    }
-
-    public ImageAbstractResponse(String message) {
-        this.message = message;
-    }
-
-    public ImageAbstractResponse(String message, int message_code) {
-        this.message = message;
-        this.message_code = message_code;
-    }
-
-    public ImageAbstractResponse(String status, String message, int message_code) {
-        this.status = status;
-        this.message = message;
-        this.message_code = message_code;
-    }
-
-    public ImageAbstractResponse(String status, String message, int message_code, Object data) {
-        this.status = status;
-        this.message = message;
-        this.message_code = message_code;
-        this.data = data;
-    }
-
-    public ImageAbstractResponse(String message, Object data) {
-        this.message = message;
-        this.data = data;
+    public ImageAbstractResponse(Object imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
