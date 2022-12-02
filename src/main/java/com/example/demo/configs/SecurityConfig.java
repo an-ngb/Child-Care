7b3035eb7c7465843f4c261e3cbda5bbe622e531
Nguyen Ba An <ashley@Nguyens-MacBook-Pro.local>
2022-12-03 00:49:15 +0700

    commit

--- src/main/java/com/example/demo/configs/SecurityConfig.java
+++ src/main/java/com/example/demo/configs/SecurityConfig.java
@@ -50,13 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable();
     }
 
     @Bean
